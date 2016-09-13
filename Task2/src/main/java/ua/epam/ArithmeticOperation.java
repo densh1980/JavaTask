6842c60d07cb36500c21d27c2a7e3aef9aa126f2
Denys Shmyhin <Denys_Shmyhin@epam.com>
2016-09-13 15:53:21 +0300

    Initial project commit

---
+++ Task2/src/main/java/ua/epam/ArithmeticOperation.java
@@ -0,0 +1,9 @@
+package ua.epam;
+
+
+public class ArithmeticOperation {
+    public double add(double value1,double value2){
+
+        return  value1+value2;
+    }
+}
---
+++ Task2/src/test/java/ua/epam/ArithmeticOperationTest.java
@@ -0,0 +1,14 @@
+package ua.epam;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+public class ArithmeticOperationTest {
+    @Test
+    public void add() throws Exception {
+
+    }
+
+}
