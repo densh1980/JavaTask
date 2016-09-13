090d435e44b36b528f66e79fb2e87c66c7dcad05
Denys Shmyhin <Denys_Shmyhin@epam.com>
2016-09-13 17:09:16 +0300

    add tests  for mul()  deduct() div()

--- Task2/src/test/java/ua/epam/ArithmeticTest.java
+++ Task2/src/test/java/ua/epam/ArithmeticTest.java
@@ -13,5 +13,23 @@ public class ArithmeticTest {
         double res =a.add(3,7);
         if(res !=10) Assert.fail();
     }
+    @Test
+    public void testDeduct() throws Exception {
+        Arithmetic a = new Arithmetic();
+        double res =a.deduct(3,7);
+        if(res !=-4) Assert.fail();
+    }
+    @Test
+    public void testMult() throws Exception {
+        Arithmetic a = new Arithmetic();
+        double res =a.mult(3,7);
+        if(res !=21) Assert.fail();
+    }
+    @Test
+    public void testDiv() throws Exception {
+        Arithmetic a = new Arithmetic();
+        double res =a.div(10,5);
+        if(res !=2) Assert.fail();
+    }
 
 }
