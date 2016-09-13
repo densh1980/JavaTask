8346a0a841ee7738639efb22661d97848249023b
Denys Shmyhin <Denys_Shmyhin@epam.com>
2016-09-13 17:50:32 +0300

    extract test object init   to  runT() using @Before

--- Task2/src/test/java/ua/epam/ArithmeticTest.java
+++ Task2/src/test/java/ua/epam/ArithmeticTest.java
@@ -1,34 +1,41 @@
 package ua.epam;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 
 public class ArithmeticTest {
+
+    private Arithmetic a;
+
+    @Before
+    public  void runT() {
+        a = new Arithmetic();
+    }
+
     @Test
     public void testAdd() throws Exception {
-        Arithmetic a = new Arithmetic();
-        double res =a.add(3,7);
+        double res = a.add(3,7);
         Assert.assertEquals(10,res,0);
-
     }
+
+
+
     @Test
     public void testDeduct() throws Exception {
-        Arithmetic a = new Arithmetic();
         double res =a.deduct(3,7);
         Assert.assertEquals(-4,res,0);
     }
     @Test
     public void testMult() throws Exception {
-        Arithmetic a = new Arithmetic();
         double res =a.mult(3,7);
         Assert.assertEquals(21,res,0);
     }
     @Test
     public void testDiv() throws Exception {
-        Arithmetic a = new Arithmetic();
         double res =a.div(10,3);
         Assert.assertEquals(3.3,res, 0.04);
     }
